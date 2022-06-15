58c39f00c01504433ccdbf6d0708645521c46011
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-06-15 10:27:09 +0530

    update

---
+++ dotandkey-clone/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Logo } from './Logo';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+
+    </ChakraProvider>
+  );
+}
+
+export default App;
